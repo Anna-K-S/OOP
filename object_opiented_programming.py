a4ba3129e986b67e3ad5776964bcc614d8520f59
Anna-K-S <anna.sergeevna1622@gmail.com>
2022-12-04 02:27:25 +0300

    OOP

---
+++ object_opiented_programming.py
@@ -0,0 +1,15 @@
+# help(list)
+my_list = [1, 2, 3, 4]
+# создан объект класса list, который содержит в себе указанные значения
+# помещаем этот объект в переменную 'my_list'
+# вызов встроенных методов
+my_list.append(5)
+print(my_list)
+
+
+class MyFirstClass:
+    pass
+
+
+object_of_my_class = MyFirstClass()
+print(type(object_of_my_class))
